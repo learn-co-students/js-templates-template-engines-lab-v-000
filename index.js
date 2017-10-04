100cff864d6e553e69e1576e106521a1d6279962
szeidman <sam.zeidman@gmail.com>
2017-10-04 13:40:00 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+function createPost(){
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+}
+
+function postComment(){
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  
+}
