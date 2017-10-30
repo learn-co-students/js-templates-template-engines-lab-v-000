6785f548411ddee92649518b10e2061ca4de709c
gb23 <gregbenj@hotmail.com>
2017-10-30 15:13:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Create a template with an id of page-template that will represent the page of a blog post, including a sidebar element and a post element that will hold our blog post.
-// Create a post-template that will represent the blog post itself. It should have a header for the post title and a footer for the post author, and be wrapped in an article element.
-// Create a comments-template that will be the container for the collection of comments on the blog post, as well as the comment form.
-// Create a comment-template that will hold an individual comment. Should have a footer for the commenter name.
-// Provide a form with an id of post-form to create a blog post with a title, body, and author. Submitting the form should build the page template with the post inside. Make the form submit using a createPost() function.
-// Provide a form within the comments-template to post comments, including commenter name and comment text. These should use the template to insert individual comments into the comments section. You should be able to keep adding more comments. Make the form submit use a postComment() function.
-
 function createPost() {
     const postTitle = document.getElementById("postTitle").value;
     const postBody = document.getElementById("postBody").value;
