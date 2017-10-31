7d264a910c22750d682687e6a9a21b93cef4bdee
skmcloughlin <skmcloughlin@gmail.com>
2017-10-31 09:57:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function createPost() {
-  
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+  var pageTemplateFn = _.template(pageTemplate);
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplateFn();
+
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  var postTemplateFn = _.template(postTemplate);
+}
+
+function postComment() {
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  var commentTemplateFn = _.template(commentTemplate);
 }
