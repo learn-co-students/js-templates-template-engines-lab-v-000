1f9519085ef86479b4df7287903f88992858072a
laramontana <larabrightside@gmail.com>
2017-11-12 21:34:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-
 function createPost() {
   var postTitle = document.getElementById("postTitle").value;
   var postText = document.getElementById("postText").value;
   var postAuthor = document.getElementById("postAuthor").value;
 
-  var postTemplate = document.getElementById("post-template").innerHTML;
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-  var templateFn = _.template(postTemplate);
 
-  var pageDiv = document.getElementByClassName("main");
+  var commentsHTML = commentsTemplate();
+  var pageHTML = pageTemplate();
+  var postHTML = postTemplate({ 'postTitle': postTitle, 'postAuthor': postAuthor, 'postText': postText });
 
-  var templateHTML = templateFn({ 'postTitle': postTitle, 'postAuthor': postAuthor, 'postText': postText });
+  var mainDiv = document.getElementsByTagName("main")[0];
+  mainDiv.innerHTML += pageHTML;
+  mainDiv.innerHTML += commentsHTML;
+  var postDiv = document.getElementById("post");
+  postDiv.innerHTML += postHTML;
+}
 
-  pageDiv.innerHTML += templateHTML;
+function postComment() {
+  var commenterName = document.getElementById("commenterName").value;
+  var commentText = document.getElementById("commentText").value;
+
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  var commentHTML = commentTemplate({ 'commentText': commentText, 'commenterName': commenterName});
+  var mainDiv = document.getElementsByTagName("main")[0];
+  mainDiv.innerHTML += commentHTML;
 }
