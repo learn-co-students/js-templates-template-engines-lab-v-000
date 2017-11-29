07fd68331cabfc23b0e4cf0167465668f53c3692
¨anniezh12¨ <aniqaelahi@yahoo.com>
2017-11-28 21:08:27 -0800

    Html template without data

---
+++ index.js
@@ -0,0 +1,13 @@
+function createPost()
+{
+   var title = getElementById('title');
+   var body = getElementById('body');
+   var author = getElementById('author');
+}
+
+function postComment()
+{
+  var pageTemplate = document.getElementById('page-template');
+  var postTemplate = document.getElementById('post-template');
+
+}
