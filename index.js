9f49127edc704c5f066319d50b7262835343fcb9
¨anniezh12¨ <aniqaelahi@yahoo.com>
2017-11-29 20:40:40 -0800

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function createPost()
 {
-   var title = getElementById('title');
-   var body = getElementById('body');
-   var author = getElementById('author');
-}
+
+  var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  var postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+   // getting post values
+   var title = document.getElementById('title').value;
+   var body = document.getElementById('body').value;
+   var author = document.getElementById('author').value;
+
+   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+   var blog = postTemplate({'title': title,'body': body,'author': author});
+   var comments = commentsTemplate();
+   var postDiv = document.getElementById('post')
+    postDiv.innerHTML += blog;
+
+   postDiv.getElementsByTagName('footer')[0].innerHTML = comments;
+ }
 
 function postComment()
 {
-  var pageTemplate = document.getElementById('page-template');
-  var postTemplate = document.getElementById('post-template');
+   var commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
+   var comment = document.getElementById("comment").value;
+   var commenter = document.getElementById("commenter").value;
+
+   var commentSection = document.getElementById("comment");
+   commentSection.innerHTML += commentTemplate({'comment':comment,'commenter': commenter});
 
 }
