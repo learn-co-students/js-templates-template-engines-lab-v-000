ee2079b0dbe507c167c466bd0103782733a89d28
Adam Tropp <adt6261@gmail.com>
2018-01-08 19:25:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function postComment() {
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  var templateFn = _.template(commentTemplate)
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
-  var commentTemplate = document.getElementById("comment-template").innerHTML;
-  var templateFn = _.template(commentTemplate);
   var commentsDiv = document.getElementById("comments");
   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
   commentsDiv.innerHTML += templateHTML;
@@ -12,16 +12,20 @@ function createPost() {
   var title = document.getElementById("postTitle").value;
   var post = document.getElementById("postBody").value;
   var poster = document.getElementById("postAuthor").value;
+
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
-  var commentTemplate = document.getElementById("comment-template").innerHTML;
+  var commentTemplate = document.getElementById("comments-template").innerHTML;
+
   var pageFn = _.template(pageTemplate);
   var postFn = _.template(postTemplate);
   var commentsFn = _.template(commentTemplate);
+
   document.getElementsByTagName("main")[0].innerHTML += pageFn();
   var commentsSection = commentsFn();
   var postDiv = document.getElementById("post");
   var blogPost = postFn({ 'title': title, 'post': post, 'author': poster });
   postDiv.innerHTML += blogPost;
   postDiv.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+  debugger;
 }
