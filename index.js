62d20f5227289690aff19d07cc88a978cb247c9a
github username <github email address>
2018-01-10 21:39:35 -0500

    .

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function createPost() {
   var pageTemplate = _.template(document.getElementById('page-template'))
   var postTemplate = _.template(document.getElementById('post-template'))
   var commentsTemplate = _.template(document.getElementById('comments-template'))
-
   debugger;
+  document.getElementsByTagName('main')[0] += pageTemplate()
 }
