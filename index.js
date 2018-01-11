a58d21ead2a4ca2a23834310f07eaf4f5ee304e4
github username <github email address>
2018-01-10 22:31:37 -0500

    .

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function createPost() {
-  var pageTemplate = _.template(document.getElementById('page-template'))
-  var postTemplate = _.template(document.getElementById('post-template'))
-  var commentsTemplate = _.template(document.getElementById('comments-template'))
-  debugger;
-  document.getElementsByTagName('main')[0] += pageTemplate()
+  const pageTemplate = _.template(document.getElementById('page-template').innerHTML)
+  const postTemplate = _.template(document.getElementById('post-template').innerHTML)
+  const commentsTemplate = _.template(document.getElementById('comments-template').innerHTML)
+
+  if (document.getElementsByTagName('main')[0].children[1]) {
+    document.getElementsByTagName('main')[0].innerHTML = pageTemplate()
+  } else {
+    document.getElementsByTagName('main')[0].innerHTML += pageTemplate()
+  }
+
+  const postObj = {
+    title: document.getElementById('post-title').value,
+    body: document.getElementById('post-body').value,
+    author: document.getElementById('post-author').value
+  }
+
+  document.getElementById('post').innerHTML = postTemplate(postObj)
+
+  // put commentsTemplate into footer
+
 }
