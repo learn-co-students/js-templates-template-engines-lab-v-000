d2a2a86dbf026dc632a1a0e9d32d9376e77f6dd9
github username <github email address>
2018-01-10 23:14:17 -0500

    .

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ function createPost() {
   const postTemplate = _.template(document.getElementById('post-template').innerHTML)
   const commentsTemplate = _.template(document.getElementById('comments-template').innerHTML)
 
-debugger;
-  if (document.getElementsByTagName('main')[0].children[1]) {
-    document.getElementsByTagName('main')[0].children[1] = pageTemplate()
-  } else {
-    document.getElementsByTagName('main')[0].innerHTML += pageTemplate()
-  }
-
-
   const postObj = {
     title: document.getElementById('post-title').value,
     body: document.getElementById('post-body').value,
     author: document.getElementById('post-author').value
   }
 
+  if (document.getElementsByTagName('main')[0].children[1]) {
+    document.getElementsByTagName('main')[0].children[1] = pageTemplate()
+  } else {
+    document.getElementsByTagName('main')[0].innerHTML += pageTemplate()
+  }
 
   let postHTML = document.getElementById('post')
 
