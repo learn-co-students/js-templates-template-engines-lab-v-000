952a8b0aedb6110c6804348b0f87447c6540719f
github username <juliannne@gmail.com>
2018-01-11 23:06:42 -0500

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function createPost() {
   var title = document.getElementById("title").value;
   var body = document.getElementById("body").value;
   var author = document.getElementById("author").value;
+
   var postTemplate = document.getElementById("post-template").innerHTML;
   var commentsTemplate = document.getElementById("comments-template").innerHTML;
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var templatePost = _.template(postTemplate);
   var templateComments = _.template(commentsTemplate);
   var templatePage = _.template(pageTemplate);
+
   document.getElementsByTagName("main")[0].innerHTML += templatePage();
   var templateHTML = templatePost({ 'title': title, 'body': body, 'author': author });
   var postDiv = document.getElementById("post");
@@ -18,7 +20,9 @@ function createPost() {
 function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
+
   var commentTemplateFn = _.template(document.getElementById("comment-template").innerHTML);
+  
   var commentsDiv = document.getElementById("comments");
   var templateHTML = commentTemplateFn({ 'comment': comment, 'commenter': commenter });
   commentsDiv.innerHTML += templateHTML;
