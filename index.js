1ef3f55280735b6646bfc74e3c4f5f68ff11df1e
Terry Blue <terryblue@comcast.net>
2018-02-15 00:11:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 
 function createPost() {
 
+  var title = document.getElementById("postTitle").value;
+  var body = document.getElementById("postBody").value;
+  var author = document.getElementById("postAuthor").value;
+  debugger;
+  //create template strings
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+  var postTemplate = document.getElementById("post-template").innerHTML;
+
+  //create page template function
+  var pageTemplateFn = _.template(pageTemplate);
+  var sidebarDiv = document.getElementById("sidebar");
+  var postDiv = document.getElementById("post");
+
+  //create post template function
+  var postTemplateFn = _.template(postTemplate);
+
 }
 
 function postComment() {
 
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  debugger;
+  //create template strings
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+
 }
+
+
+//execute template function with JSON object for the interpolated values
+//var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+//append rather than replace!
+//commentsDiv.innerHTML += templateHTML;
