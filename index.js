3cf152861928579bec9f49a7c656ba9190bf271f
Terry Blue <terryblue@comcast.net>
2018-02-15 05:35:14 +0000

    Done.

--- index.js
+++ index.js
@@ -4,31 +4,38 @@ function createPost() {
   var title = document.getElementById("postTitle").value;
   var body = document.getElementById("postBody").value;
   var author = document.getElementById("postAuthor").value;
-  debugger;
+
   //create template strings
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
 
   //create page template function
   var pageTemplateFn = _.template(pageTemplate);
-  var sidebarDiv = document.getElementById("sidebar");
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplateFn();
+  var commentTemplateFn = _.template(commentsTemplate);
+
+  var commentsSelection = commentTemplateFn();
+  //var sidebarDiv = document.getElementById("sidebar");
   var postDiv = document.getElementById("post");
 
   //create post template function
   var postTemplateFn = _.template(postTemplate);
-
+  var postSection = postTemplateFn({ 'title': title, 'body': body, 'author': author })
+  postDiv.innerHTML = postSection;
+  postHtml.getElementsByTagName("footer")[0].innerHTML = commentsSelection;
 }
 
 function postComment() {
 
-  var commenter = document.getElementById("commenterName").value;
-  var comment = document.getElementById("commentText").value;
-  debugger;
+  //create template function
+  var templateFn = _.template(commentTemplate);
+
   //create template strings
   var commentTemplate = document.getElementById("comment-template").innerHTML;
 
-  //create template function
-  var templateFn = _.template(commentTemplate);
+  var commenter = document.getElementById("commenter").value;
+  var comment = document.getElementById("commentText").value;
 
   var commentsDiv = document.getElementById("comments");
 
