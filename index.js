2ea5831ce428a194093c420189bcbbd56be0c621
Learn <huffjd1@gmail.com>
2018-03-14 14:22:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function code(){
-  return "Hi"
-};
+function postComment(){
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML)
+  var commentText = document.getElementById("commentText").value 
+  var commenterName = document.getElementById("commenterName").value 
+  
+  var commentsSection = document.getElementById("comments") 
+  commentsSection.innerHTML += commentTemplate({'commenterName': commenterName, 'comment': commentText})
+}
+
+function createPost(){
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML) 
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
+
+  var postTitle = document.getElementById("postTitle").value 
+  var postContent = document.getElementById("postContent").value 
+  var postAuthor = document.getElementById("postAuthor").value 
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate() 
+
+  var blogSection = postTemplate({'title': postTitle, 'body': postContent, 'poster': postAuthor})
+  var commentsSection = commentsTemplate() 
+  var postElement = document.getElementById("post")
+
+  postElement.innerHTML = blogSection 
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection 
+}
