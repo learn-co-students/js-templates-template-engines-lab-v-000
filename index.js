81b9009063d0bb1c92ea540b45dfcd3830bfef94
github username <github email address>
2018-03-16 19:35:10 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function createPost() {
+  var postTitle = document.getElementById("postTitle").value;
+  var postBody = document.getElementById("postBody").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+}
+
+function postComment() {
+
+}
