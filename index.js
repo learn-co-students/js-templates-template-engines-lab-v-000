b6ca4f1bd1f6991dc3da7e441ab939f202a2e0f5
mevanstron <michael@michaelgregoryevans.com>
2018-03-29 10:13:41 -0500

    Done.

--- index.js
+++ index.js
@@ -3,30 +3,35 @@ function createPost() {
   const body = document.getElementById("post-body").value;
   const author = document.getElementById("post-author").value;
 
-  // console.log(title)
-  // console.log(body)
-  // console.log(author)
+  const pageTemplate = document.getElementById("page-template").innerHTML;
+  const pageTemplateFn = _.template(pageTemplate);
+  const pageTemplateHTML = pageTemplateFn();
+
+  let main = document.body.querySelector("main");
+  main.innerHTML += pageTemplateHTML;
 
   const postTemplate = document.getElementById("post-template").innerHTML;
+  const postTemplateFn = _.template(postTemplate);
+  const postTemplateHTML = postTemplateFn({'title': title, 'body': body, 'author': author});
 
-  const templateFn = _.template(postTemplate);
+  let postDiv = document.getElementById("post");
+  postDiv.innerHTML += postTemplateHTML;
 
-  const postDiv = document.getElementById("post");
+  const commentsTemplate = document.getElementById("comments-template").innerHTML;
+  const commentsTemplateFn = _.template(commentsTemplate);
+  const commentsTemplateHTML = commentsTemplateFn();
 
-  const templateHTML = templateFn({'title': title, 'body': body, 'author': author});
-
-  return templateHTML.innerHTML;
-  //console.log("hello");
+  postDiv.innerHTML += commentsTemplateHTML;
 }
 
-function createPage() {
-  const pageTemplate = document.getElementById("page-template").innerHTML;
+function postComment() {
+  const commenter = document.getElementById("commenter-name").value;
+  const comment = document.getElementById("comment-text").value;
 
-  const templateFn = _.template(pageTemplate);
+  const commentTemplate = document.getElementById("comment-template").innerHTML;
+  const commentTemplateFn = _.template(commentTemplate);
+  const commentTemplateHTML = commentTemplateFn({'commenter': commenter, 'comment': comment});
 
-  const body = document.body;
-
-  const templateHTML = templateFn({'post': createPost()});
-
-  body.innerHTML+= templateHTML;
+  let commentsDiv = document.getElementById("comments");
+  commentsDiv.innerHTML += commentTemplateHTML;
 }
