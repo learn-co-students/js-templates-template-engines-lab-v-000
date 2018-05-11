8e5ae27a59ef38dfda85edb296d992040317b40a
Allyson Wesman <allysonwesman@gmail.com>
2018-05-10 20:22:26 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function createPost() {
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  let title = document.getElementById("postTitle").value;
+  let body = document.getElementById("postBody").value;
+  let author = document.getElementById("postAuthor").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  let blogSection =  postTemplate({'title': title, 'body': body, 'author': author});
+  let commentsSection = commentsTemplate();
+  let postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection
+  postElement.getElementsByTagName("footer")[0].innerHTML += commentsSection;
+}
+
+function postComment() {
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  let commenter = document.getElementById("commenterName").value;
+  let comment = document.getElementById("commentContent").value;
+
+  let commentsSection= document.getElementById("comments")
+
+  commentsSection.innerHTML += commentTemplate({'comment': comment, 'commenter': commenter});
+}
