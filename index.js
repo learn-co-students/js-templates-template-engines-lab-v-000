83f36c6bb1ecbc383fb67a16f82e90cd83ea61f1
jacobsilver2 <jacobsilver2@mac.com>
2018-05-11 10:29:45 -0400

    third

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function createPost() {
+  // template functions
+  var pageTemplate = _.template(document.getElementById('page-template').innerHTML)
+  var postTemplate = _.template(document.getElementById('post-template').innerHTML)
+  var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML)
+
+  //get the values
+  var postTitle = document.getElementById('postTitle');
+  var postBody = document.getElementById('postBody');
+  var postAuthor = document.getElementById('postAuthor');
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var blogSection = postTemplate({ 'title': postTitle, 'body': postBody, 'post': postAuthor });
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById();
 
 }
+
+
+
 
 function postComment() {
   var commenter = document.getElementById("commenterName").value;
@@ -12,3 +30,12 @@ function postComment() {
   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
   commentsDiv.innerHTML += templateHTML;
 }
+
+
+
+
+//   var postElement = document.getElementById("post");
+
+//   postElement.innerHTML = blogSection;
+//   postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+// }
