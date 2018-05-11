23e8c62c4cc96e0eb7a3087273c6d8b7c99f6eec
jacobsilver2 <jacobsilver2@mac.com>
2018-05-11 10:06:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function createPost() {
+
+}
+
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = 'document.getElementById("comment-template").innerHTML;';
+  var templateFn = _.template(commentTemplate);
+  var commentsDiv = document.getElementById("comments");
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+  commentsDiv.innerHTML += templateHTML;
+}
