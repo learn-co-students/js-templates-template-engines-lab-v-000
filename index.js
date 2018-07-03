f707ff9958423482922cca21f1b58ec891652120
Sean Oughton <seanoughton@gmail.com>
2018-07-03 17:35:34 +0000

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ function createPost() {
   // Get the data submitted by the form
   var title = document.getElementById("postTitle").value;
   var body = document.getElementById("postBody").value;
-  var title = document.getElementById("postTitle").value;
+  var author = document.getElementById("postAuthor").value;
 
-  //
+  //Create template
   var pageTemplate = document.getElementById("page-template").innerHTML;
+  var postTemplate = document.getElementById("post-template").innerHTML
+  var templateFn = _.template(postTemplate);
 
-  var templateFn = _.template(pageTemplate);
+  //Get the place where you are going to spit the html back out
+  var postsDiv = document.getElementById("posts");
 
-  var postDiv = document.getElementById("comments");
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'postTitle': title, 'postBody': body, 'postAuthor': author });
+
+
+  //Return the template back to the html
+
+  postsDiv.innerHTML += templateHTML;
+
+
 }
