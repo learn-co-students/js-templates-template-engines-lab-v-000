5d3410ecb556aa46152532e9c9c0423dc5249f74
Sean Oughton <seanoughton@gmail.com>
2018-07-03 18:15:15 +0000

    Done.

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ function createPost() {
   var postTitle = document.getElementById("postTitle").value;
   var post = document.getElementById("postBody").value;
   var postAuthor = document.getElementById("postAuthor").value;
-
   //Create template
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
 
+
+  //this gets the main DOM element and adds to it's innerHTML the page template html that is returned from the function
+   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+
   //Get the place where you are going to spit the html back out
     //this is inside the pageTemplate, the div with the id of post
   var postElement = document.getElementById("post");
-
   //execute template function with JSON object for the interpolated values
-   var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
 
 
 
