fe5cab5859c984b1d93d54c25b8b2e61f81a57c9
Richard Steinbach <steinbach.dick@gmail.com>
2018-07-03 11:29:46 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
+
+function createPost(){
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment(){
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+}
