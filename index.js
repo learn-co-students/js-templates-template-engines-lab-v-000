70da654aa7155a63ffa7a530f462beeea5622ec0
Katleiah Ramos <katleiahramos@gmail.com>
2018-07-26 20:43:58 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function createPost(){
+  var postTitle  = document.getElementById("postTitle").value;
+  var postBody  = document.getElementById("postBody").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+
+  var postTemplate = document.getElementById("posts")
+}
