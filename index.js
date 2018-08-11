38dfc7cdc9c64d62da0b4c36e86621044c3ef710
Dee Race <deerace12@gmail.com>
2018-08-11 19:32:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 function createPost(){
+  // create template functions
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
+  // get blog values
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let body = document.getElementById("body").value;
+
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
   let postSection = postTemplate({"title": title, "body": body, "author": author});
   let commentsSection = commentsTemplate();
   let post = document.getElementById("post");
+
   post.innerHTML = postSection;
   post.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
 function postComment() {
   let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
   let commenterName = document.getElementById("commenterName").value;
   let commentText = document.getElementById("commentText").value:
+
   let comments = document.getElementById("comments");
   comments.innerHTML += commentTemplate({'comment': commentText, 'commenter': commenterName});
 }
