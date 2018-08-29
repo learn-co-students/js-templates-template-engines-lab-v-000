11b1a6f7338f9814d9af499b884acfeb86890da6
taryn yaeger <tsyaeger@gmail.com>
2018-08-29 17:22:38 +0000

    Done.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function createPost() {
 
   var postTemplate = document.getElementById("post-template").innerHTML;
  
-  var templateFn = _.template(posttTemplate);
+  var templateFn = _.template(postTemplate);
  
   var postsDiv = document.getElementById("posts");
  
