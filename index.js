fd2c3f6da3c6d8b2519dd5e2c1b73118d129a002
taryn yaeger <tsyaeger@gmail.com>
2018-08-29 19:24:24 +0000

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ function createPost() {
   var poster = document.getElementById("posterName").value;
   var post = document.getElementById("postText").value;
  
-
   var postTemplate = document.getElementById("post-template").innerHTML;
- 
-  var templateFn = _.template(postTemplate);
+  var postTemplateFn = _.template(postTemplate);
  
   var postsDiv = document.getElementById("posts");
- 
   var templateHTML = templateFn({'postTitle': postTitle, 'post': post, 'poster': poster });
- 
   postsDiv.innerHTML += templateHTML;
+  
+  
+  
+  var pageTemplate = document.getElementById("page-template").innerHTML
+  
+  var pageTemplateFn = _.template(pageTemplate);
+
 }
