0d75bdc536345f7094bc3291fe832f5ff32e12da
taryn yaeger <tsyaeger@gmail.com>
2018-08-29 23:56:52 +0000

    Done.

--- index.js
+++ index.js
@@ -5,29 +5,37 @@ function createPost() {
  
   var postTemplate = document.getElementById("post-template").innerHTML;
   var postTemplateFn = _.template(postTemplate);
+  
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
+  var commentsTemplateFn = _.template(commentsTemplate);
+  var commentsTemplateHTML = commentsTemplateFn()
+  
  
   var postsDiv = document.getElementById("posts");
   var postTemplateHTML = postTemplateFn({'postTitle': postTitle, 'post': post, 'poster': poster });
-  postsDiv.innerHTML += postTemplateHTML;
+  postsDiv.innerHTML = postTemplateHTML;
+  postsDiv.innerHTML += commentsTemplateHTML;
   
   
   
-  // var pageTemplate = document.getElementById("page-template").innerHTML
-  // var pageTemplateFn = _.template(pageTemplate);
+  
+  var pageTemplate = document.getElementById("page-template").innerHTML
+  var pageTemplateFn = _.template(pageTemplate);
 
 }
 
 
 function postComment() {
-  var commentText = document.getElementById("commentText");
-  var commenter = document.getElementById("commenterName");
+  debugger
+  
+  var commentText = document.getElementById("commentText").value;
+  var commenter = document.getElementById("commenterName").value;
   
   var commentTemplate = document.getElementById("comment-template").innerHTML;
   var commentTemplateFn = _.template(commentTemplate);
   
   var commentsDiv = document.getElementById("comments");
-  var commentTemplateHTML = commentTemplateFn({'commentText': comment, 'commenter': commenter });
+  var commentTemplateHTML = commentTemplateFn({'commentText': commentText, 'commenter': commenter }).innerHTML;
   commentsDiv.innerHTML += commentTemplateHTML;
-  
 }
 
