c338ed4487ed526bbb75b2fcd4c0101d0ade6e2b
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-04 19:15:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const createPost = function() {
+  var postName = document.getElementById("post-name")
+  var postContent = document.getElementById("post-content")
+  var postTemplate = document.getElementById("post-template")
+}
+
+const postComment = function() {
 
 }
