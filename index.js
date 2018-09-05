61557505ba7021df150d746dca07d199677cd231
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-05 15:13:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const createPost = function () {
-  var postName = document.getElementById("post-name").value
-  var postContent = document.getElementById("post-content").value
-  var poster = document.getElementById("poster").value
+function createPost() {
+  var postName = document.getElementById("post-name").value;
+  var postContent = document.getElementById("post-content").value;
+  var poster = document.getElementById("poster").value;
 
-  var postTemplate = _.template(document.getElementById("post-template").innerHTML)
-  var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
-  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML)
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
-  var postHTML = postTemplate({'title': postName, 'body': postContent, 'poster': poster})
+  var postHTML = postTemplate({'title': postName, 'body': postContent, 'poster': poster});
 
-  var postElement = document.getElementById("post")
-  postElement.innerHTML = postHTML
-  document.getElementByTagName("main")[0].innerHTML += pageTemplate()
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+  var postElement = document.getElementById("post");
+  postElement.innerHTML += postHTML;
+
 }
 
-const postComment = function () {
+function postComment() {
   var commenter = document.getElementById(".comments-template .comments-form .commenter").value
   var comment = document.getElementById(".comments-template .comments-form .comment").value
 
