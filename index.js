99fd41b7d397b5b5ba314cbe4b6454998bdd1910
mstmari <michael.landrum.st.marie@gmail.com>
2018-09-06 22:03:53 -0400

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,41 @@ function createPost(){
   var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   var postTemplate = _.template(document.getElementById('post-template').innerHTML);
   var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+
+  var commentsSection = commentsTemplate();
+
+  var postElement = document.getElementById("post");
+
+postElement.innerHTML = blogSection;
+postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+
+
 }
 
 function postComment(){
-  var commenterName = _.template(document.getElementById('commenterName').innerHTML);
-  var comment = _.template(document.getElementById('comment').innerHTML);
+  debugger
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+
 }
 
 
-var postTitle = document.getElementById('postTitle').value;
-
-var postAuthor = document.getElementById('postAuthor').value;
-
-var post = document.getElementById('post').value;
+// var postTitle = document.getElementById('postTitle').value;
+//
+// var postAuthor = document.getElementById('postAuthor').value;
+//
+// var post = document.getElementById('post').value;
