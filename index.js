820fc623490e60a3db8d00498bfd733a8a25ca5c
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-10 22:03:46 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function createPost() {
   //append rather than replace!
   postDiv.innerHTML += templateHTML;
 }
+
+function postComment() {
+  event.preventDefault();
+  let comment = document.getElementById("comment_content").value;
+  let commentTemplate = document.getElementById("comment-template").innerHTML;
+  //create template function
+  let templateFn = _.template(commentTemplate);
+  let commentDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  let templateHTML = templateFn({ 'comment': comment });
+
+  //append rather than replace!
+  commentDiv.innerHTML += templateHTML;
+}
