1e9dc43007c660ac2e8012f356938666d8264b5f
github username <github email address>
2018-09-19 19:41:13 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function createPost() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+
+  //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
+  //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
