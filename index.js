9427fc93a94a041df30ebde98c865fb4b1739c00
Zac Willmington <zac.willmington@gmail.com>
2018-09-21 17:14:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 
 
 function createPost() {
-    let pageTemplate = document.getElementById("page-template").innerHTML;
-
     let author = document.getElementById('author').value;
     let post = document.getElementById('post').value;
+    let title = document.getElementById('title').value;
 
 
+    let pageTemplate = document.getElementById("page-template").innerHTML;
     let postTemplate = document.getElementById("post-template").innerHTML;
+    let commentsTemplate = document.getElementById("comments-template").innerHTML;
 
-
-    let templateFn = _.template(postTemplate);
-    
-    let templateHTML = templateFn({'author': author, 'post': post});
-
-
-    pageTemplate + templateHTML;
+    let postTemplateFn = _.template(postTemplate);
+    let pageTemplateFn = _.template(pageTemplate);
+    let commentsTemplateFn = _.template(commentsTemplate);
 }
 
 function postComment() {
-    let commenterName = document.getElementById('commenterName').value;
-    let  comment = document.getElementById('comment').value;
-
-    let commentTemplate = document.getElementById('comment-template').innerHTML;
-
-    let templateFn = _.template(commentTemplate);
-
-    let commentsDiv = document.getElementById('.comments');
-
-    let templateHTML = templateFn({'comment': comment, 'commenterName': commenterName});
-
-    commentsDiv.innerHTML + templateHTML;
+    let commentTemplate = document.getElementById("comment-template").innerHTML;
+   let templateFn = _.template(commentTemplate);
 }
