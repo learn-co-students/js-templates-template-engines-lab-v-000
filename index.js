76ed15338ad838c26b0343c7285f8ee3a9b372be
MFeuer23 <mallory.feuer@gmail.com>
2018-10-03 21:11:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 function postComment() {
-  let commenter = document.getElementById('commenterName').value;
+  let commenter = document.getElementById('commenterName');
+  console.log(commenter);
   let comment = document.getElementById('commentText').value;
- 
+
   //insert comment into "comments" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
- 
+
   //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
   //let commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
   let commentTemplate = document.getElementById('comment-template').innerHTML;
- 
+
   //create template function
   let templateFn = _.template(commentTemplate);
- 
+
   let commentsDiv = document.getElementById('comments-template');
- 
+
   //execute template function with JSON object for the interpolated values
   let templateHTML = templateFn({ comment: comment, commenter: commenter });
- 
+
   //append rather than replace!
   commentsDiv.innerHTML += templateHTML;
 }
 
 function createPost() {
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
+
+
   let title = document.getElementById('postTitle').value;
   let body = document.getElementById('postBody').value;
   let author = document.getElementById('postAuthor').value;
-  
-  let postTemplate = document.getElementById('post-template').innerHTML;
-  
-  let templateFn = _.template(postTemplate);
 
-  let postDiv = document.getElementById('page-template');
- 
-  //execute template function with JSON object for the interpolated values
-  let templateHTML = templateFn({ title: title, body: body, author: author});
- 
-  //append rather than replace!
-  postDiv.innerHTML += templateHTML;
+  let templateHTML = postTemplate({ "title": title, "body": body, "author": author});
+  let postHTML = pageTemplate({ "post": templateHTML });
+
+  document.getElementsByTagName('main')[0].innerHTML += postHTML;
+  //
+  // let commentsSection = commentsTemplate();
+  // var postElement = document.getElementById('post')
+  //
+  // postElement.innerHTML += templateHTML
+
 }
