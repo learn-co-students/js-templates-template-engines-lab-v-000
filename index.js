a97fde64dfcac35d1220f8d23144e93fc3370635
github username <github email address>
2018-10-17 14:21:52 -0600

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function createPost(){
   let author = document.getElementById('author').value
   let content = document.getElementById('content').value
 
-  let commentTemplate = _.template(document.getElementById('comments-template').innerHtml)
-  let pageTemplate = _.template(document.getElementById('page-template').innerHtml)
-  let postTemplate = _.template(document.getElementById('post-template').innerHtml)
+  let commentTemplate = _.template(document.getElementById('comments-template').innerHTML)
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML)
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML)
 
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
@@ -13,7 +13,8 @@ function createPost(){
   let commentSection = commentTemplate()
   let postElement = document.getElementById('post')
 
-  postElement.innerHTML = blogSection + commentSection
+  postElement.innerHTML = blogSection
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentSection;
 
 }
 
@@ -21,9 +22,9 @@ function postComment(){
   let commenter = document.getElementById('commenter').value
   let commentText = document.getElementById('commentText').value
 
-  let commentTemplate = _.template(document.getElementById('comment-template').innerHtml)
+  let commentTemplate = _.template(document.getElementById('comment-template').innerHTML)
   let commentDiv = document.getElementById('comments')
   let templateHTML = commentTemplate({commenter: commenter, commentText: commentText})
 
-  commentDiv.innerHtml += templateHTML
+  commentDiv.innerHTML += templateHTML
 }
