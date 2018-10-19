ef69a47059c6935d4d997a412ccc630318185ed7
Kevin Huang <yellow.khaos@gmail.com>
2018-10-19 21:14:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function createPost() {
-  let postTemplate = document.getElementById('post-template').innerHTML
-  let pageTemplate = document.getElementById('page-template').innerHTML
-  let postTemplateFn = _.template(postTemplate)
-  let pageTemplateFn = _.template(pageTemplate)
+
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML)
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML)
+  let commentTemplate = _.template(document.getElementById('comments-template').innerHTML)
+
+  let postTitle = document.getElementById('postTitle').value
+  let postBody = document.getElementById('postBody').value
+  let postAuthor = document.getElementById('postAuthor').value
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate()
+
+  let postDiv = document.getElementById("post")
+
+  let postHTML = postTemplate({postAuthor: postAuthor, postBody: postBody, postTitle: postTitle})
+
+  postDiv.innerHTML += postHTML
+
+  document.getElementsByTagName("footer")[0].innerHTML +=commentTemplate()
+
+
 }
 
 function postComment() {
-  let commentTemplate = document.getElementById('comment-template').innerHTML
-  let commentTemplateFn = _.template(commentTemplate)
+  let commentTemplate = _.template(document.getElementById('comment-template').innerHTML)
+
+  let commentDiv = document.getElementById("comments")
+  let commentBody = document.getElementById('commentText').value
+  let commenter = document.getElementById('commenter').value
+
+  commentDiv.innerHTML += commentTemplate({commentBody:commentBody, commentAuthor: commenter})
+
 }
