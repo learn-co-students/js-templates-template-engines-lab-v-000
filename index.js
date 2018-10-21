78eef565a25379fcfe44e070fd90b1b2d8a3f52d
theresamorelli <theresa.l.morelli@gmail.com>
2018-10-21 16:08:21 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function createPost() {
+  let pageFn = _.template(document.getElementById('page-template').innerHTML);
+  let postFn = _.template(document.getElementById('post-template').innerHTML);
+}
+
+function postComment() {
+  let comment = _.template(document.getElementById('comment-template').innerHTML);
+}
