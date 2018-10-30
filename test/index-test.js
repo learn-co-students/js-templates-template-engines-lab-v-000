a7e8bae9ace1e132c06f12d37652aed963d94fcc
roitblatari <roitblatari@gmail.com>
2018-10-30 13:58:57 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -31,6 +31,7 @@ describe('index', () => {
       it('builds the proper templates', () => {
         createPost()
         var spy = expect.spyOn(window._, 'template').andReturn(function(){})
+
         postComment()
         expect(spy).toHaveBeenCalledWith(document.getElementById("comment-template").innerHTML)
       })
