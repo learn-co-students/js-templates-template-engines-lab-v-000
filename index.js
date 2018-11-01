edb139357d85edd55c5551275f738d1f8ba22f1f
amckean12 <amckean@me.com>
2018-11-01 17:11:29 -0500

    Done.

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ function createPost() {
   let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   let postTemplate = _.template(document.getElementById("post-template").innerHTML);
   let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-
-  //post Values
-
-  let postTitle = document.getElementById("postTitle").value;
-  let postAuthor = document.getElementById("postAuthor").value;
-  let post = document.getElementById("postBody").value;
 }
 
 function postComment() {
