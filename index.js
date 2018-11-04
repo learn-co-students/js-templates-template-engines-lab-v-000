35f50b1f4cf6655dff0ae119a340f6486893abf3
zep1994 <snaxbags@protonmail.com>
2018-11-04 16:34:12 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+function createPost() {
+  let title = document.getElementById('postTitle').value;
+  let body = document.getElementById('postBody').value;
+  let author = document.getElementById('postAuthor').value;
+
+  let pageTemplate  
+}
