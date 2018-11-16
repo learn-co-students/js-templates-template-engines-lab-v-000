4b1590c08315ddeff83bb1b2a1816f3cafcd38c5
Carol Greene <carolgreene3@gmail.com>
2018-11-16 01:08:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 function createPost () {
 
-  //Build template functions
-  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
-  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  //Build template strings
+  let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
+  let postTemplate = _.template(document.getElementById('post-template').innerHTML);
+  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML)
 
 //get blog values
-  let postTitle = document.getElementById("postTitle").value;
-  let postAuthor = document.getElementById("postAuthor").value;
-  let post = document.getElementById("postBody").value;
+let postTitle = document.getElementById('postTitle').value
+let postAuthor = document.getElementById('postAuthor').value
+let postBody = document.getElementById('postBody').value
 
-  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-  let blogSection = postTemplate({'title': postTitle, 'body': postBody, 'author': postAuthor});
-  let commentsSection = commentsTemplate();
-  let postElement = document.getElementById("post");
+let blogSection = postTemplate({'title': postTitle, 'body': postBody, 'author': postAuthor});
+let commentsSection = commentsTemplate();
+let postElement = document.getElementById("post");
 
-  postElement.innerHTML = blogSection;
-  postElement.getElementsByTagName('footer')[0].innerHTML = commentsSection;
-
+postElement.innerHTML = blogSection;
+postElement.getElementsByTagName('footer')[0].innerHTML = commentsSection
 }
 
 function postComment () {
   //Build template function
-  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  let commentTemplate = _.template(document.getElementById('comment-template').innerHTML);
+
 
   //get comments values
-  let commentText = document.getElementById("commentText").value;
-  let commenterName = document.getElementById("commenter").value;
-
-  let commentsSection = document.getElementById("comments");
+  let commenterName = document.getElementById('commenter').value;
+  let commentText = document.getElementById('commentText').value;
+  let commentsSection = document.getElementById('comments');
 
   commentsSection.innerHTML += commentTemplate({'commenter': commenterName, 'comment': commentText})
-
 }
