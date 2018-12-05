e1ef4aa27def97ea56feaaa7707451f21afad710
AlexFriedland <friedlandaj@gmail.com>
2018-12-04 21:05:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function createPost() {
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  var commentsTempalte = _.template(document.getElementById("comments-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value;
@@ -9,5 +9,20 @@ function createPost() {
 
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
+  var blogSection = postTemplate({'title': postTitle, 'body': post, 'poster': postAuthor});
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
 
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
