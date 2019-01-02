458f2f7418204acbf01fc218c56df4eba450348a
alcesalces11331 <rollin.metzger@gmail.com>
2019-01-01 16:35:11 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function createPost() {
+    var postAuthor = document.getElementById("postAuthor").value;
+    var postTitle = document.getElementById("postTitle").value;
+    var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+    var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+    var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+    document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+    var blogSection = postTemplate({'postTitle': postTitle, 'postBody': postBody, 'postAuthor': postAuthor});
+    var commentsSection = commentsTemplate();
+    var postElement = document.getElementById("post");
+
+    postElement.innerHTML = blogSection;
+    postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment() {
+    var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+    var commentText = document.getElementById("commentText").value;
+    var commenterName = document.getElementById("commenter").value;
+    var commentsSection = document.getElementById("comments");
+
+    commentsSection.innerHTML += commentTemplate({'commenter': commenterName, 'comment': commentText});
+}
