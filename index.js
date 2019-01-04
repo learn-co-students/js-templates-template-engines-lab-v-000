a63c2879b1e63c3b432c1f30563a2c850f72fd81
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-04 19:39:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 function createPost() {
-  let title = document.getElementById('postTitle').value;
-  let body = document.getElementById('postBody').value;
-  let author = document.getElementById('postAuthor').value;
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-  let postTemplate = document.getElementById('')
+  let title = document.getElementById("postTitle").value;
+  let body = document.getElementById("postBody").value;
+  let author = document.getElementById("postAuthor").value;
 
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  let blogSection =  postTemplate({'title': title, 'body': body, 'author': author});
+  let commentsSection = commentsTemplate();
+  let postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection
+  postElement.getElementsByTagName("footer")[0].innerHTML += commentsSection;
 
 }
 
 
 function postComment() {
-  let commenter = document.getElementById('commenterName').value;
-  let comment = document.getElementById('commentText').value;
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
-  let commentTemplate = document.getElementById('comment-template').innerHTML;
+  let commenter = document.getElementById("commenterName").value;
+  let comment = document.getElementById("commentContent").value;
 
-  let templateFn = _.template(commentTemplate);
+  let commentsSection= document.getElementById("comments")
 
-  let commentsDiv = document.getElementById('comments');
-
-  let templateHTML = templateFn({comment: comment, commenter: commenter});
-
-  commentsDiv.innerHTML += templateHTML;
+  commentsSection.innerHTML += commentTemplate({'comment': comment, 'commenter': commenter});
 }
