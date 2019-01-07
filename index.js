0ad123655266d18269d827d9d963c00a08fd028f
BrittanyTinnin <bntinnin7@gmail.com>
2019-01-07 16:41:33 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function createPost() {
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+}
+
+function postComment() {
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+}
