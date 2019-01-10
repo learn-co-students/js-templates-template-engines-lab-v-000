e22ee43fd45660bf58afaf4c0c268969da3fd55b
Lucy Wall <lhwall@gmail.com>
2019-01-10 21:21:46 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ let postText = document.getElementById("postText").value
 let author = document.getElementById("postAuthor").value
 
 let postTemplate = document.getElementById("post-template").innerHTML
+let pageTemplate = document.getElementById("page-template").innerHTML
+let commentsTemplate = document.getElementById("comments-template").innerHTML
+let commentTemplate = document.getElementById("comment-template").innerHTML
+
 let templateFn = _.template(postTemplate)
 
 let postsDiv = document.getElementById("posts")
@@ -12,3 +16,18 @@ let templateHTML = templateFn({title: title, postText: postText, author: author}
 
 postsDiv.innerHTML += templateHTML
   }
+
+
+  function postComment(){
+  let commentText = document.getElementById("commentText").value
+  let commenterName = document.getElementById("commenterName").value
+
+  let commentTemplate = document.getElementById("comment-template").innerHTML
+  let templateFn = _.template(commentTemplate)
+
+  let commentsDiv = document.getElementById("comments")
+
+  let templateHTML = templateFn({commentText: commentText, commenterName: commenterName})
+
+  commentsDiv.innerHTML += templateHTML
+    }
