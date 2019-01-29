ddcdb708d2eb1de57b0d1d9ca27ddc1654207093
lauradarlak <lauradarlak@gmail.com>
2019-01-29 16:06:53 -0500

    Done.

--- index.js
+++ index.js
@@ -17,21 +17,23 @@ function postComment() {
 }
 
 function createPost() {
+  //  create template functions
   let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   let postTemplate = _.template(document.getElementById('post-template').innerHTML);
   let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
 
+  // get blog values
   let title = document.getElementById('postTitle').value;
   let body = document.getElementById('postBody').value;
   let author = document.getElementById('postAuthor').value;
 
-  let mainDiv = document.querySelector("main");
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-  //execute template function with JSON object for the interpolated values
-  let postTemplateHTML = postTemplate({ title: title, body: body, author: author });
-  let commentsTemplateHTML = commentsTemplate();
-  let pageTemplateHTML = pageTemplate({ post: postTemplateHTML });
+  var blogSection = postTemplate({ 'title': title, 'body': body, 'author': author });
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
 
-  mainDiv.innerHTML +=(pageTemplate + commentsTemplateHTML);
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 
 }
