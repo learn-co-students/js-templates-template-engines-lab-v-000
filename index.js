b592f9eecb4500884fa694c55a63e625d1a092e9
amitnyc83 <amit_nyc@live.com>
2019-02-11 16:17:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-function createPost(){
+// function createPost(){
+//
+//   // get values
+//   let postTitle = document.getElementById("postTitle").value;
+//
+//   let postBody = document.getElementById("postBody").value;
+//
+//   let postAuthor = document.getElementById("postAuthor").value;
+//
+//   // create template Functions
+//
+//   let pageTemplate = document.getElementById("page-template").innerHTML;
+//
+//   let postTemplate = document.getElementById("post-template").innerHTML;
+//
+//   let commentsTemplate = document.getElementById("comments-template").innerHTML;
+//
+//   let commmentTemplate = document.getElementbyId("comment-template").innerHTML;
+//
+//
+//   //create template
+//   let postTemplateFn = _.template(postTemplate);
+//   let pageTemplateFn = _.template(pageTemplate);
+//
+//
+//   let postDiv = document.getElementById("post-id");
+//
+//   let pageTemplateHTML = pageTemplateFn;
+//   let templateHTML = postTemplateFn({ title: postTitle, body: postBody, author: postAuthor});
+//
+//   // append
+//   postDiv.innerHTML += templateHTML;
+// }
 
-  // get values
-  let postTitle = document.getElementById("postTitle").value;
+function createPost() {
+  // templates
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-  let postBody = document.getElementById("postBody").value;
+   // get values for templates
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
 
-  let postAuthor = document.getElementById("postAuthor").value;
+   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-  // create template Functions
+   var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
 
-  let pageTemplate = document.getElementById("page-template").innerHTML;
-
-  let postTemplate = document.getElementById("post-template").innerHTML;
-
-  let commentsTemplate = document.getElementById("comments-template").innerHTML;
-
-
-  //create template
-  let postTemplateFn = _.template(postTemplate);
-  let pageTemplateFn = _.template(pageTemplate);
-
-
-  let postDiv = document.getElementById("post-id");
-
-  let templateHTML = postTemplateFn({ title: postTitle, body: postBody, author: postAuthor});
-
-  // append
-  postDiv.innerHTML += templateHTML;
+   postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
-
 
 
 
@@ -41,7 +64,7 @@ function postComment() {
   let comment = document.getElementById('commentText').value;
 
   //create template function for post comment
-  let commentTemplate = document.getElementById('comment-template').innerHTML;
+  let commentTemplate = document.getElementById("comment-template").innerHTML;
 
   let templateFn = _.template(commentTemplate);
 
