80719280a04dfb1b9f58616fae2b28d0e33a5492
freeWillee <will.ik.lee@gmail.com>
2019-02-14 17:00:33 -0700

    Done.

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ function createPost() {
     let body = document.getElementById('postbody').value;
     let author = document.getElementById('postAuthor').value;
 
-    let pageTemplate = document.getElementById('page-template')
+    // grab the templates from lodash script file
+
+    let pageTemplate = document.getElementById('page-template').innerHTML;
+    let postTemplate = document.getElementById('post-template').innerHTML;
     
     let pageTemplateFunction = _.template(pageTemplate);
+    let postTemplateFunction = _.template(postTemplate);
+
+    let pageHTML = pageTemplateFunction();
+    let postHTML = postTemplateFunction( { title: title, body: body, author: author });
+
 
     let mainTag = document.getElementsByTagName('main')
-
-    let pageHTML = pageTemplateFunction()
-    
     mainTag.innerHTML += pageHTML;
 }
 
-function submitComment() {
+function postComment() {
 
 }
