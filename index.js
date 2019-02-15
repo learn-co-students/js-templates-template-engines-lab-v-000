7e62e8ba748b019bd056b3fd8bbe45595d706520
freeWillee <will.ik.lee@gmail.com>
2019-02-15 09:50:06 -0700

    Done.

--- index.js
+++ index.js
@@ -45,5 +45,41 @@ function postComment() {
 
     // hold onto template function to be called later;
     let commentHTML = commentFunction( { comment: comment, commenter: commenter });
+    
+    // append templates to main HTML element (execution phase)
     commentsDiv.innerHTML += commentHTML;
 }
+
+
+// ANSWER: 
+
+// function createPost() {
+//     // create template functions
+//     var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+//     var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+//     var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  
+//     // get blog values
+//     var postTitle = document.getElementById("postTitle").value;
+//     var postAuthor = document.getElementById("postAuthor").value;
+//     var post = document.getElementById("postBody").value;
+  
+//     document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+  
+//     var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+//     var commentsSection = commentsTemplate();
+//     var postElement = document.getElementById("post");
+  
+//     postElement.innerHTML = blogSection;
+//     postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+//   }
+  
+//   function postComment() {
+//     var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  
+//     var commentText = document.getElementById("commentText").value;
+//     var commenterName = document.getElementById("commenter").value;
+  
+//     var commentsSection = document.getElementById("comments");
+//     commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
+//   }
