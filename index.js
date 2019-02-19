501cbf02b51910da92d9638c7b006ed8383a7316
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-19 22:58:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+function createPost() {
+  let title = document.querySelector("#postTitle");
+  let body = document.querySelector("#postBody");
+  let author = document.querySelector("#postAuthor");
+}
