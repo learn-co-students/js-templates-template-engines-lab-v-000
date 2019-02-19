187c1cd534a6a560dab6db00a853fee5d4caebb5
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-19 23:03:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function createPost() {
-  let title = document.querySelector("#postTitle");
-  let body = document.querySelector("#postBody");
-  let author = document.querySelector("#postAuthor");
+  let title = document.querySelector("#postTitle").value;
+  let body = document.querySelector("#postBody").value;
+  let author = document.querySelector("#postAuthor").value;
+
+  let postTemplate = "<div class="post"><h1><%= title %></h1><p><%= body %></p><p>Posted by: <%= author %></p>";
+
+  
 }
