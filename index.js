4e53710cefc8f6c78fca40e93a17e0155c172787
github username <github email address>
2019-02-20 12:51:32 -0500

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function createPost() {
   let title = document.getElementById('blogTitle').value;
   let body = document.getElementById('blogBody').value;
   let author = document.getElementById('blogAuthor').value;
+  let post = document.getElementById('post-form').value;
 
-  let pageTemplate = '<div>postTemple</div>';
-  let pageTemplateFN = _.template(pageTemplate);
+  let pageTemplate ='<div id="page-template"><%= post %></div>';
+  let pageTemplateFn = _.template(pageTemplate);
+
   let postTemplate = '<article><header><%= title %></header><p><%= body %></p><footer><%= author %></footer></article>';
-  let templateFn = _.template(postTemplate);
+  let postTemplateFn = _.template(postTemplate);
 }
